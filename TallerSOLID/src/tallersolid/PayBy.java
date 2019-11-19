1750669384962c6968a434f148255b460a4cd923
nicoleagila <nagila@espol.edu.ec>
2019-11-19 13:23:00 -0500

    Interfaz payment y clase payBy que separa los tipos de pago

---
+++ TallerSOLID/src/tallersolid/PayBy.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallersolid;
+
+/**
+ *
+ * @author nicoleagila
+ */
+public class PayBy implements Payment{
+
+    @Override
+    public void pay_card() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pay_cash() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pay_PayPal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ TallerSOLID/src/tallersolid/Payment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallersolid;
+
+/**
+ *
+ * @author nicoleagila
+ */
+public interface Payment {
+    void pay_card();
+    void pay_cash();
+    void pay_PayPal();
+}
