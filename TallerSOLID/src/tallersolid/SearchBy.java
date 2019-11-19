2a821a01d8286d1ba00df61d5ab1ba2d56878156
kcast19 <52088505+kcast19@users.noreply.github.com>
2019-11-19 13:22:17 -0500

    Cambiar interfaz de busqueda

---
+++ TallerSOLID/src/tallersolid/SearchBy.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallersolid;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kelly
+ */
+public class SearchBy implements Search{
+
+    @Override
+    public ArrayList<Book> searchByAuthor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Book> searchByTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Book> searchByCategory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
